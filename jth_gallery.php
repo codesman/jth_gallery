253bf9a51b4f784153a4d40102f7561963f005f4
Tom Holland <tom@thpro.net>
2014-08-21 07:25:36 -0700

    Added TDD Deps and set up shortcode

--- jth_gallery.php
+++ jth_gallery.php
@@ -5,7 +5,7 @@
  * Description: Adds custom gallery shortcode
  * Version:     0.1.0
  * Author:      Tom Holland
- * Author URI:  
+ * Author URI:
  * License:     GPLv2+
  * Text Domain: jth
  * Domain Path: /languages
@@ -13,17 +13,14 @@
 
 /**
  * Copyright (c) 2014 Tom Holland (email : tom@thpro.net)
- *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License, version 2 or, at
  * your discretion, any later version, as published by the Free
  * Software Foundation.
- *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
@@ -38,15 +35,19 @@
  */
 
 // Composer autoload
-// modified to work with PHP 5.2 thanks to 
+// modified to work with PHP 5.2 thanks to
 // https://bitbucket.org/xrstf/composer-php52
 include 'vendor/autoload_52.php';
 
 /**
  * Activation and deactivation
-*/
-register_activation_hook(__FILE__, array('Jth_Gallery', 'activate'));
-register_deactivation_hook(__FILE__, array('Jth_Gallery', 'deactivate'));
+ */
+register_activation_hook( __FILE__, array( 'Jth_Gallery',
+                                           'activate'
+) );
+register_deactivation_hook( __FILE__, array( 'Jth_Gallery',
+                                             'deactivate'
+) );
 
 class Jth_Gallery
 {
@@ -56,40 +57,57 @@ class Jth_Gallery
     public $prefix = null;
     public $js_assets = null;
     public $css_assets = null;
-    
+
     /**
      * An instance of the plugin main class, meant to be singleton.
-     *
      * @var Jth_Gallery
      */
     private static $instance = null;
-    
+
     /**
      * The global functions adapter used to isolate the class.
-     *
      * @var tad_FunctionsAdapter or a mock object.
      */
     private $f = null;
-    
-    public function __construct(tad_FunctionsAdapterInterface $f = null)
+
+    public function __construct( tad_FunctionsAdapterInterface $f = null )
     {
-        if (is_null($f)) {
+        if ( is_null( $f ) )
+        {
             $f = new tad_FunctionsAdapter();
         }
         $this->f = $f;
-    }
 
+        add_filter( 'post_gallery', array( __CLASS__, 'my_gallery_shortcode'), 10, 4 );
+    }
+    public function my_gallery_shortcode( $output = '', $atts, $content = false, $tag = false )
+    {
+        $return = $output; // fallback
+
+        var_dump($atts);
+        $output = str_replace('>', ' rel="gallery-gallery">', $output);
+        // retrieve content of your own gallery function
+        $my_result =  $output;
+
+        // boolean false = empty, see http://php.net/empty
+        if( !empty( $my_result ) ) {
+            $return = $my_result;
+        }
+
+        return $return;
+    }
     /**
      * Do not allow writing access to properties.
      *
      * @param string $name
-     * @param mixed $value
+     * @param mixed  $value
      */
-    public function __set($name, $value){
-        trigger_error(sprintf('Cannot set %s->%s property.',  __CLASS__, $name));
+    public function __set( $name, $value )
+    {
+        trigger_error( sprintf( 'Cannot set %s->%s property.', __CLASS__, $name ) );
     }
 
-    public static function the($key)
+    public static function the( $key )
     {
         return self::$instance;
     }
@@ -102,8 +120,8 @@ class Jth_Gallery
     private function init_vars()
     {
         $this->version = '0.1.0';
-        $this->path = dirname(__FILE__);
-        $this->uri = $this->f->plugin_basename(__FILE__ );
+        $this->path = dirname( __FILE__ );
+        $this->uri = $this->f->plugin_basename( __FILE__ );
         $this->prefix = "jth";
         $this->js_assets = $this->uri . '/assets/js';
         $this->css_assets = $this->uri . '/assets/css';
@@ -117,25 +135,26 @@ class Jth_Gallery
      */
     public static function init()
     {
-        if (self::$instance == null) {
+        if ( self::$instance == null )
+        {
             self::$instance = new self();
         }
         self::$instance->init_vars();
         self::$instance->hook();
 
-        $locale = apply_filters('plugin_locale', get_locale(), 'jth');
-        load_textdomain('jth', WP_LANG_DIR . '/jth/jth-' . $locale . '.mo');
-        load_plugin_textdomain('jth', false, dirname(plugin_basename(__FILE__)) . '/languages/');
+        $locale = apply_filters( 'plugin_locale', get_locale(), 'jth' );
+        load_textdomain( 'jth', WP_LANG_DIR . '/jth/jth-' . $locale . '.mo' );
+        load_plugin_textdomain( 'jth', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
     }
 
     /**
      * Hook into actions and filters here
-     *
+
      */
     public function hook()
     {
-
     }
+
     /**
      * Activate the plugin
      */
@@ -143,6 +162,7 @@ class Jth_Gallery
     {
         // do something here
     }
+
     /**
      * Deactivate the plugin
      * Uninstall routines should be in uninstall.php
